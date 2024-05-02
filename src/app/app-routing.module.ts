f9af647108b3c729628009393f933b35a168ca1b
BORIS HECTOR ARRIAGADA ROJAS <106414902+Barriagada666@users.noreply.github.com>
2024-05-02 15:46:54 -0400

    avanzando login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'landing',
     pathMatch: 'full'
   },
   {
@@ -22,6 +22,22 @@ const routes: Routes = [
   {
     path: 'modal-modificar',
     loadChildren: () => import('./pages/modal-modificar/modal-modificar.module').then( m => m.ModalModificarPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'signup',
+    loadChildren: () => import('./pages/signup/signup.module').then( m => m.SignupPageModule)
+  },
+  {
+    path: 'landing',
+    loadChildren: () => import('./pages/landing/landing.module').then( m => m.LandingPageModule)
+  },
+  {
+    path: 'reset-password',
+    loadChildren: () => import('./pages/reset-password/reset-password.module').then( m => m.ResetPasswordPageModule)
   },
 ];
 
